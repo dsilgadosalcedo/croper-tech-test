68c2148a611b936f2a3d27c19546bbeb58e1c8c5
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2025-07-31 21:59:59 -0500

    Add MongoDB configuration and Docker setup, update environment variables and validation schema

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,19 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: process.env.PORT,
+  port: parseInt(process.env.PORT || '3000', 10),
+  database: {
+    name: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
+  },
+  mongo: {
+    dbName: process.env.MONGO_DB,
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    password: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    port: process.env.MONGO_PORT
+      ? parseInt(process.env.MONGO_PORT, 10)
+      : undefined,
+    host: process.env.MONGO_HOST,
+    connection: process.env.MONGO_CONNECTION,
+  },
 }));
--- src/config/validation.schema.ts
+++ src/config/validation.schema.ts
@@ -1,5 +1,18 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  PORT: Joi.number(),
+  // Application
+  PORT: Joi.number().required(),
+
+  // Database
+  DATABASE_NAME: Joi.string().optional(),
+  DATABASE_PORT: Joi.string().optional(),
+
+  // MongoDB
+  MONGO_DB: Joi.string().optional(),
+  MONGO_INITDB_ROOT_USERNAME: Joi.string().optional(),
+  MONGO_INITDB_ROOT_PASSWORD: Joi.string().optional(),
+  MONGO_PORT: Joi.number().optional(),
+  MONGO_HOST: Joi.string().optional(),
+  MONGO_CONNECTION: Joi.string().optional(),
 });
